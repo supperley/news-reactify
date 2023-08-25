ce7083110a6dc4fae1bd6845e356982f1107db03
supperley <v.bobrovko@gmail.com>
2023-08-25 11:39:53 +0300

    initial commit

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import NewsBanner from '../../components/NewsBanner/NewsBanner';
+import styles from './styles.module.css';
+import { getNews } from '../../api/apiNews';
+import NewsList from '../../components/NewsList/NewsList';
+
+const Main = () => {
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getNews();
+        setNews(response.news);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNews();
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      {news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : null}
+      {!isLoading && <NewsList news={news} />}
+    </main>
+  );
+};
+
+export default Main;
