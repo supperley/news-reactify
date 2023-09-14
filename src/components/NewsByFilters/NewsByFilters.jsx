77877ce6ecb67905e1a5f6b8ec49ebf1761f4f62
supperley <v.bobrovko@gmail.com>
2023-09-14 18:01:32 +0300

    add design for web

---
+++ src/components/NewsByFilters/NewsByFilters.jsx
@@ -0,0 +1,46 @@
+import styles from './styles.module.css';
+import NewsList from '../../components/NewsList/NewsList';
+import Pagination from '../../components/Pagination/Pagination';
+import { TOTAL_PAGES } from '../../constants/constants';
+import NewsFilters from '../NewsFilters/NewsFilters';
+
+const NewsByFilters = ({ filters, changeFilter, isLoading, news }) => {
+  const handleNextPage = () => {
+    if (filters.page_number < TOTAL_PAGES) {
+      changeFilter('page_number', filters.page_number + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (filters.page_number > 1) {
+      changeFilter('page_number', filters.page_number - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber) => {
+    changeFilter('page_number', pageNumber);
+  };
+
+  return (
+    <section className={styles.section}>
+      <NewsFilters filters={filters} changeFilter={changeFilter} />
+      <Pagination
+        currentPage={filters.page_number}
+        totalPages={TOTAL_PAGES}
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+      />
+      <NewsList isLoading={isLoading} news={news} />
+      <Pagination
+        currentPage={filters.page_number}
+        totalPages={TOTAL_PAGES}
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+      />
+    </section>
+  );
+};
+
+export default NewsByFilters;
