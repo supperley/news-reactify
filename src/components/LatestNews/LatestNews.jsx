d49fe64a76ecfd7393a51dbbfdb93d285d93460d
supperley <v.bobrovko@gmail.com>
2023-09-30 17:14:06 +0300

    add latest news

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,10 +1,14 @@
 import BannersList from '../BannersList/BannersList';
 import styles from './styles.module.css';
+import { useFetch } from '../../helpers/hooks/useFetch';
+import { getLatestNews } from '../../api/apiNews';
 
-const LatestNews = ({ banners, isLoading }) => {
+const LatestNews = () => {
+  const { data, isLoading } = useFetch(getLatestNews);
+
   return (
     <section className={styles.section}>
-      <BannersList banners={banners} isLoading={isLoading} />
+      <BannersList banners={data && data.news} isLoading={isLoading} />
     </section>
   );
 };
